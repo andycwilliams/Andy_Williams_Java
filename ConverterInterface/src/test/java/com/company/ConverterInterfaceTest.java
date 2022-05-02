672fcb37c520a7d484199a7b1d52c6b03fb6a22a
Andy <awilliamscomposing@gmail.com>
2022-05-01 19:33:55 -0700

    Reworking interface integration.

---
+++ ConverterInterface/src/test/java/com/company/ConverterInterfaceTest.java
@@ -0,0 +1,30 @@
+package com.company;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ConverterInterfaceTest {
+//    ConverterInterface convertTest;
+//
+//    @Before
+//    public void setUp() {
+//        convertTest = new ConverterInterface();
+//    }
+//
+//    @Test
+//    public void shouldReturnAMonth() {
+//
+//        assertEquals(3, calc.divide(6,2));
+//        assertEquals(2, calc.divide(80, 39));
+//        assertEquals(1, calc.divide(105, 100));
+//    }
+//
+//    @Test
+//    public void shouldReturnADay() {
+//
+//        assertEquals(3, calc.divide(6,2));
+//        assertEquals(2, calc.divide(80, 39));
+//        assertEquals(1, calc.divide(105, 100));
+//    }
+
+}
