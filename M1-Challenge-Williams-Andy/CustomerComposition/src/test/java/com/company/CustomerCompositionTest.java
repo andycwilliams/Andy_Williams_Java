05fea5a8470b57c4c78d6706f664e0b9c617c611
Andy <awilliamscomposing@gmail.com>
2022-05-05 22:33:42 -0700

    Prepping to submit.

--- M1-Challenge-Williams-Andy/CustomerComposition/src/test/java/com/company/CustomerCompositionTest.java
+++ M1-Challenge-Williams-Andy/CustomerComposition/src/test/java/com/company/CustomerCompositionTest.java
@@ -10,22 +10,33 @@ public class CustomerCompositionTest {
     @Before
     public void SetUp() throws Exception {
         customer = new Customer();
+
+        customer.setFirstName("John");
+        customer.setLastName("Sprinkles");
+        customer.setEmailAddress("email@address.net");
+        customer.setPhoneNo(1234567890);
+        customer.setRewardsMember(true);
+
+        customer.setAddress("Bovine Industries");
+        customer.setStreet1("1234 Pork Street");
+        customer.setStreet2("1106");
+        customer.setCity("Burger City");
+        customer.setState("Beef State");
     }
 
     @Test
-    public void shouldReturnSameCustomer() {
+    public void shouldReturnCustomerFirstName() {
         assertEquals("John", customer.getFirstName());
+    }
+
+    @Test
+    public void shouldReturnCustomerPhoneNo() {
+        assertEquals(true, customer.getRewardsMember());
+    }
+
+    @Test
+    public void shouldReturnCustomerBillingAddress() {
+        assertEquals("Bovine Industries", customer.getAddress());
     }
 }
 
-//        JohnS.setFirstName("John");
-//                JohnS.setLastName("Sprinkles");
-//                JohnS.setEmailAddress("email@address.net");
-//                JohnS.setPhoneNo(1234567890);
-//                JohnS.setRewardsMember(true);
-//
-//                JohnS.setAddress("Bovine Industries");
-//                JohnS.setStreet1("1234 Pork Street");
-//                JohnS.setStreet2("1106");
-//                JohnS.setCity("Burger City");
-//                JohnS.setState("Beef State");
