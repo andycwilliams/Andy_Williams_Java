03b85dd7b9e2de0e7c6a8e540f44ad65ff2301a8
Andy <awilliamscomposing@gmail.com>
2022-05-31 14:48:28 -0700

    Microservices set up as Maven projects. Dividing up source code.

---
+++ M5-Challenge-Williams-Andy/gamestore-catalog/src/main/java/com/company/CatalogService/repository/ConsoleRepository.java
@@ -0,0 +1,12 @@
+package com.company.CatalogService.repository;
+
+import com.trilogyed.gamestore.model.Console;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ConsoleRepository extends JpaRepository<Console, Long> {
+    List<Console> findAllByManufacturer(String manufacturer);
+}
